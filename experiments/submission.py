c424e25c3e2447a0f4ac91e897f8e3873444a12c
upura <upura0@gmail.com>
2021-05-09 03:32:49 +0900

    Submission

---
+++ experiments/submission.py
@@ -0,0 +1,38 @@
+import os
+import boto3
+from pathlib import Path
+from datetime import datetime
+from dotenv import load_dotenv
+
+
+def upload_submission(local_file: str, task: str):
+    print("Starting submission at {}...\n".format(datetime.utcnow()))
+    # env info should be in your env file
+    BUCKET_NAME = os.getenv("BUCKET_NAME") # you received it in your e-mail
+    PARTICIPANT_ID = os.getenv("PARTICIPANT_ID") # you received it in your e-mail
+    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY") # you received it in your e-mail
+    AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY") # you received it in your e-mail
+
+    # instantiate boto3 client
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY ,
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name="us-west-2",
+    )
+    s3_file_name = os.path.basename(local_file)
+    s3_file_path = "{}/{}/{}".format(task, PARTICIPANT_ID, s3_file_name)  # it needs to be like e.g. "rec/id/*.json"
+    s3_client.upload_file(local_file, BUCKET_NAME, s3_file_path)
+    print("\nAll done at {}: see you, space cowboy!".format(datetime.utcnow()))
+
+
+def submission(outfile_path: Path, task: str) -> None:
+    # load envs from env file
+    load_dotenv(verbose=True, dotenv_path="upload.env")
+    EMAIL = os.getenv("EMAIL") # the e-mail you used to sign up
+    replaced_email = EMAIL.replace("@", "_")
+    current_datetime_ms = int(datetime.utcnow().timestamp() * 1000)
+    parent = outfile_path.parent
+    submit_file_path = parent / f"{replaced_email}_{current_datetime_ms}.json"
+    outfile_path.rename(submit_file_path)
+    upload_submission(local_file=str(submit_file_path), task=task)
---
+++ experiments/weighted_averaging.py.py
@@ -0,0 +1,64 @@
+import sys
+import json
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from scipy.optimize import minimize
+from scipy.stats import rankdata
+from sklearn.metrics import f1_score
+
+sys.path.append('../')
+from ayniy.utils import Data
+from submission import submission
+
+
+def load_from_run_id(run_id: str, to_rank: False):
+    oof = Data.load(f'../output/pred/{run_id}-train.pkl')
+    pred = Data.load(f'../output/pred/{run_id}-test.pkl')
+    if to_rank:
+        oof = rankdata(oof) / len(oof)
+        pred = rankdata(pred) / len(pred)
+    return (oof, pred)
+
+
+def f(x, y_true, y_pred):
+    score = f1_score(y_true, y_pred >x) * -1
+    return score
+
+
+if __name__ == '__main__':
+
+    run_ids = [
+        'run000',
+        'run001',
+        'run002',
+        'run003',
+        'run004',
+        'run005'
+    ]
+    run_name = 'weight000'
+
+    data = [load_from_run_id(ri, to_rank=False) for ri in run_ids]
+    y_trains = [Data.load(f'../input/pickle/y_train_nb{nb}.pkl') for nb in range(0, 12, 2)]
+    thrs = [minimize(f, 0.5, args=(y_trains[idx], data[idx][0]), method='Nelder-Mead')['x'][0] for idx in range(6)]
+
+    subs = [pd.read_csv(f'../session_rec_sigir_data/prepared/sample_submission_nb{nb}.csv') for nb in range(0, 12, 2)]
+    for idx in range(6):
+        subs[idx]['label'] = (data[idx][1]>thrs[idx]).astype(int)
+    sub = pd.concat(subs, axis=0)
+    sub.index = sub['session_id_hash_']
+    sid2label = sub['label'].to_dict()
+
+    with open('../session_rec_sigir_data/test/intention_test_phase_1.json', 'r') as f:
+        original_test_data = json.load(f)
+
+    for idx, query_label in enumerate(original_test_data):
+        query = query_label["query"]
+        session_id_hash = query[0]["session_id_hash"]
+        original_test_data[idx]["label"] = sid2label[session_id_hash]
+
+    outfile_path = Path('../output/submissions') / "submission.json"
+    with open(outfile_path, 'w') as outfile:
+        json.dump(original_test_data, outfile)
+    submission(outfile_path, 'cart')
