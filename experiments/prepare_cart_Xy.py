4fb403f2ea22c9e3ab0fa910ab5fbd1645410084
upura <upura0@gmail.com>
2021-05-17 17:54:00 +0900

    Fix feature engineering

--- experiments/prepare_cart_Xy.py
+++ experiments/prepare_cart_Xy.py
@@ -2,76 +2,73 @@ import sys
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 
 sys.path.append('../')
 from ayniy.utils import Data
 from datasets import pickle_load
 
 
-def extract_features(sample_df, nb):
-    target_product_sku_hash = sample_df.loc[len(sample_df) - nb - 1]['product_sku_hash']
-    sample_df_event_product = sample_df.query('event_type=="event_product"').reset_index(drop=True)
-    sample_df_event_product.loc[:, 'product_sku_hash'] = (sample_df_event_product['product_sku_hash'] == target_product_sku_hash).astype(int).values
-    num_add_same_product = len(sample_df_event_product.query('product_action=="add" and product_sku_hash==1'))
-    num_detail_same_product = len(sample_df_event_product.query('product_action=="detail" and product_sku_hash==1'))
-    num_remove_same_product = len(sample_df_event_product.query('product_action=="remove" and product_sku_hash==1'))
-    num_add_not_same_product = len(sample_df_event_product.query('product_action=="add" and product_sku_hash==0'))
-    num_detail_not_same_product = len(sample_df_event_product.query('product_action=="detail" and product_sku_hash==0'))
-    num_remove_not_same_product = len(sample_df_event_product.query('product_action=="remove" and product_sku_hash==0'))
-    if nb == 0:
-        return (
-            num_add_same_product,
-            num_detail_same_product,
-            num_remove_same_product,
-            num_add_not_same_product,
-            num_detail_not_same_product,
-            num_remove_not_same_product,
-        )
-    num_add_same_product_nb = len(sample_df_event_product.query(f'product_action=="add" and product_sku_hash==1 and index>={len(sample_df) - nb}'))
-    num_detail_same_product_nb = len(sample_df_event_product.query(f'product_action=="detail" and product_sku_hash==1 and index>={len(sample_df) - nb}'))
-    num_remove_same_product_nb = len(sample_df_event_product.query(f'product_action=="remove" and product_sku_hash==1 and index>={len(sample_df) - nb}'))
-    num_add_not_same_product_nb = len(sample_df_event_product.query(f'product_action=="add" and product_sku_hash==0 and index>={len(sample_df) - nb}'))
-    num_detail_not_same_product_nb = len(sample_df_event_product.query(f'product_action=="detail" and product_sku_hash==0 and index>={len(sample_df) - nb}'))
-    num_remove_not_same_product_nb = len(sample_df_event_product.query(f'product_action=="remove" and product_sku_hash==0 and index>={len(sample_df) - nb}'))
-    return (
-        num_add_same_product,
-        num_detail_same_product,
-        num_remove_same_product,
-        num_add_not_same_product,
-        num_detail_not_same_product,
-        num_remove_not_same_product,
-        num_add_same_product_nb,
-        num_detail_same_product_nb,
-        num_remove_same_product_nb,
-        num_add_not_same_product_nb,
-        num_detail_not_same_product_nb,
-        num_remove_not_same_product_nb,
+def extract_product_action_count(df, nb):
+    _df = df.copy()
+    _df['target_product_sku_hash'] = _df['product_sku_hash_list'].map(lambda x: x[len(x) - nb - 1])
+    _df['product_action_list_nb'] = _df['product_action_list'].map(lambda x: x[len(x) - nb:])
+    _df['product_sku_hash_list_nb'] = _df['product_sku_hash_list'].map(lambda x: x[len(x) - nb:])
+    _df['same_product_sku_hash'] = _df.apply(
+        lambda x: [product is x['target_product_sku_hash'] for product in x['product_sku_hash_list'] if isinstance(product, str)],
+        axis=1
     )
+    _df['product_action_list'] = _df['product_action_list'].map(lambda x: [action for action in x if isinstance(action, str)])
 
+    for pc in ['add', 'detail', 'remove']:
+        _df[f'num_{pc}_same_product'] = _df.apply(
+            lambda x: len([(action, product) for action, product in zip(x['product_action_list'], x['same_product_sku_hash']) if (action==pc) and product]),
+            axis=1
+        )
+        _df[f'num_{pc}_not_same_product'] = _df.apply(
+            lambda x: len([(action, product) for action, product in zip(x['product_action_list'], x['same_product_sku_hash']) if (action==pc) and not(product)]),
+            axis=1
+        )
 
-colnames0 = [
-    'num_add_same_product',
-    'num_detail_same_product',
-    'num_remove_same_product',
-    'num_add_not_same_product',
-    'num_detail_not_same_product',
-    'num_remove_not_same_product',
-]
-colnames = [
-    'num_add_same_product',
-    'num_detail_same_product',
-    'num_remove_same_product',
-    'num_add_not_same_product',
-    'num_detail_not_same_product',
-    'num_remove_not_same_product',
-    'num_add_same_product_nb',
-    'num_detail_same_product_nb',
-    'num_remove_same_product_nb',
-    'num_add_not_same_product_nb',
-    'num_detail_not_same_product_nb',
-    'num_remove_not_same_product_nb',
-]
+    if nb == 0:
+        return _df[[
+            'num_add_same_product',
+            'num_detail_same_product',
+            'num_remove_same_product',
+            'num_add_not_same_product',
+            'num_detail_not_same_product',
+            'num_remove_not_same_product',
+        ]]
+
+    _df['same_product_sku_hash_nb'] = _df.apply(
+        lambda x: [product is x['target_product_sku_hash'] for product in x['product_sku_hash_list_nb'] if isinstance(product, str)],
+        axis=1
+    )
+    _df['product_action_list_nb'] = _df['product_action_list_nb'].map(lambda x: [action for action in x if isinstance(action, str)])
+
+    for pc in ['add', 'detail', 'remove']:
+        _df[f'num_{pc}_same_product_nb'] = _df.apply(
+            lambda x: len([(action, product) for action, product in zip(x['product_action_list_nb'], x['same_product_sku_hash_nb']) if (action==pc) and product]),
+            axis=1
+        )
+        _df[f'num_{pc}_not_same_product_nb'] = _df.apply(
+            lambda x: len([(action, product) for action, product in zip(x['product_action_list_nb'], x['same_product_sku_hash_nb']) if (action==pc) and not(product)]),
+            axis=1
+        )
+
+    return _df[[
+        'num_add_same_product',
+        'num_detail_same_product',
+        'num_remove_same_product',
+        'num_add_not_same_product',
+        'num_detail_not_same_product',
+        'num_remove_not_same_product',
+        'num_add_same_product_nb',
+        'num_detail_same_product_nb',
+        'num_remove_same_product_nb',
+        'num_add_not_same_product_nb',
+        'num_detail_not_same_product_nb',
+        'num_remove_not_same_product_nb',
+    ]]
 
 
 if __name__ == '__main__':
@@ -86,22 +83,13 @@ if __name__ == '__main__':
         X_train = df_train.groupby('session_id_hash').agg({
             'is_search': ['sum'],
             'server_timestamp_epoch_ms': ['count', np.ptp],
-            'label': ['max']
+            'label': ['max'],
+            'product_sku_hash': list,
+            'product_action': list
         }).reset_index(drop=True)
         X_train.columns = ["_".join(x) for x in X_train.columns.ravel()]
-
-        train_gpb = df_train.groupby('session_id_hash')
-        features = []
-        for tpg in tqdm(train_gpb):
-            sid, tmp_train = tpg
-            tmp_train = tmp_train.reset_index(drop=True)
-            features.append(extract_features(tmp_train, nb))
-
-        if nb == 0:
-            features = pd.DataFrame(features, columns=colnames0)
-        else:
-            features = pd.DataFrame(features, columns=colnames)
-        X_train = pd.concat([X_train, features], axis=1)
+        X_train = pd.concat([X_train, extract_product_action_count(X_train, nb)], axis=1)
+        X_train = X_train.drop(['product_sku_hash_list', 'product_action_list'], axis=1)
         Data.dump(X_train.drop('label_max', axis=1), f'../input/pickle/X_train_nb{nb}.pkl')
         Data.dump(X_train['label_max'], f'../input/pickle/y_train_nb{nb}.pkl')
 
@@ -116,21 +104,15 @@ if __name__ == '__main__':
     for nb in [0, 2, 4, 6, 8, 10]:
         print('****** Starting nb==', nb)
         X_test_nb = X_test.query(f'nb_after_add_max=={nb}').reset_index(drop=True)
-        test_gb = df_test.query(f'nb_after_add=={nb}').groupby('session_id_hash')
-        features = []
-        for tpg in tqdm(test_gb):
-            sid, tmp_test = tpg
-            tmp_test = tmp_test.reset_index(drop=True)
-            features.append(extract_features(tmp_test, nb))
-
-        if nb == 0:
-            features = pd.DataFrame(features, columns=colnames0)
-        else:
-            features = pd.DataFrame(features, columns=colnames)
-        X_test_nb = pd.concat([X_test_nb, features], axis=1)
+        X_test_nb = pd.concat([X_test_nb, extract_product_action_count(X_test_nb, nb)], axis=1)
 
         Data.dump(
-            X_test_nb.drop(['session_id_hash_', 'nb_after_add_max'], axis=1),
+            X_test_nb.drop([
+                'session_id_hash_',
+                'nb_after_add_max',
+                'product_sku_hash_list',
+                'product_action_list'
+            ], axis=1),
             f'../input/pickle/X_test_nb{nb}.pkl'
         )
         sub = X_test_nb[['session_id_hash_']]
